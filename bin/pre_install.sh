d02e2c698a18c594f6dedc4ec787c2ca9f6a465f
Tim Rau bladedpenguin <bladedpenguin@gmail.com>
2016-11-02 13:10:31 -0400

    added wait for dpkg lock to pre_install

--- bin/pre_install.sh
+++ bin/pre_install.sh
@@ -1,6 +1,8 @@
 echo "Provisioning virtual machine..."
 echo "Updating aptitude"
 
+
+apt-get update
 #Sometimes ubuntu wakes up already updating. We need to wait for that to finish.
 while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
     echo "Waiting for dpkg lock..." 
@@ -8,21 +10,11 @@ while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
 done
 #If dpkg stays locked forever, the deployment is bust anyway, so theres no point in breaking the loop. ASG lifecyccle stuff will eventually trash the instance.
 
-apt-get update -y
 
-echo "Installing Git"
-apt-get install git -y 
 
-echo "Installing Apache"
-apt-get install apache2 -y
+echo "Installing Git Apache Java"
+apt-get install -y git apache2 default-jre composer npm nodejs-legacy
 
-echo "Installing Java"
-apt-get install -y default-jre
-
-echo "Installing Redis"
-apt-get -y install redis-server
-chmod -R aou+rw /etc/redis
-chmod -R aou+rw /var/log/redis
 
 echo "Installing PHP7"
 add-apt-repository -y ppa:ondrej/php
@@ -41,16 +33,11 @@ apt-get install -y php-redis
 apt-get install -y php7.0-xml
 apt-get install -y php7.0-zip
 
-echo "Installing Composer"
-#curl -sS https://getcomposer.org/installer | /usr/bin/php -- --install-dir=/usr/bin --filename=composer
-apt-get install -y composer
 
 echo "Installing node and npm"
-apt-get install -y npm nodejs-legacy
 ln -s /usr/bin/nodejs /usr/bin/node
 
 echo "Installing bower"
-apt-get install nodejs-legacy npm
 npm install -g bower
 
 echo "Installing PHPUnit"
